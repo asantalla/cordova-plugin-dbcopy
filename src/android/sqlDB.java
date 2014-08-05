335d121eebdc011393d57143a29702ba45ef0ade
Adrián Santalla <adrian.santalla@osoco.es>
2014-08-05 08:12:57 +0200

    Created a method to check if a database is created in Android.

--- src/android/sqlDB.java
+++ src/android/sqlDB.java
@@ -19,51 +19,66 @@ public class sqlDB extends CordovaPlugin {
 	PluginResult plresult = new PluginResult(PluginResult.Status.NO_RESULT);
 	
     @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-         		
-        if (action.equalsIgnoreCase("copy")) {
-        		this.copyDB(args.getString(0), callbackContext);
-            	return true; 
-         } else {
-                plresult = new PluginResult(PluginResult.Status.INVALID_ACTION);
-                callbackContext.sendPluginResult(plresult);
-                return false;
-          }
-      }
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException { 		
+		if (action.equalsIgnoreCase("copy")) {
+			this.copyDB(args.getString(0), callbackContext);
+			return true;
+		}           
+		else if (action.equalsIgnoreCase("isDatabaseCreated")) {
+			this.isDatabaseCreated(args.getString(0), callbackContext);
+			return true;
+		}
+		else {
+			plresult = new PluginResult(PluginResult.Status.INVALID_ACTION);
+			callbackContext.sendPluginResult(plresult);
+			return false;
+		}
+	} 
     
-    private void copyDB(String dbName, final CallbackContext callbackContext)
-    {
-    	
-    	final File dbpath;
-    	   dbname = dbName;
-           final DatabaseHelper dbhelper = new DatabaseHelper(this.cordova.getActivity().getApplicationContext());
-           dbpath = this.cordova.getActivity().getDatabasePath(dbname);
-           Boolean dbexists = dbpath.exists();
-           Log.d("CordovaLog", "Inside Plugin");
-           Log.d("CordovaLog", "DatabasePath = "+dbpath+"&&&& dbname = "+dbname+"&&&&DB Exists ="+dbexists);
-           
-           if(dbexists){
-           		plresult = new PluginResult(PluginResult.Status.ERROR);
-           		callbackContext.sendPluginResult(plresult);
-           } else {
-           			cordova.getThreadPool().execute(new Runnable(){
+    private void copyDB(String dbName, final CallbackContext callbackContext) {
+		final File dbpath;
+		dbname = dbName;
+		final DatabaseHelper dbhelper = new DatabaseHelper(this.cordova.getActivity().getApplicationContext());
+		dbpath = this.cordova.getActivity().getDatabasePath(dbname);
+		Boolean dbexists = dbpath.exists();
 
-						@Override
-						public void run() {
-							PluginResult plResult = new PluginResult(PluginResult.Status.NO_RESULT);
-							// TODO Auto-generated method stub
-							try
-							{
-								dbhelper.createdatabase(dbpath);
-								plResult = new PluginResult(PluginResult.Status.OK);
-			           			callbackContext.sendPluginResult(plResult);
-							} catch (Exception e){
-								plResult = new PluginResult(PluginResult.Status.ERROR);
-			           			callbackContext.sendPluginResult(plResult);
-			           		}
-						}
-           				
-           			});
-           }
+		if (dbexists) {
+			plresult = new PluginResult(PluginResult.Status.ERROR);
+			callbackContext.sendPluginResult(plresult);
+		}
+		else {
+			cordova.getThreadPool().execute(new Runnable(){
+				@Override
+				public void run() {
+					PluginResult plResult = new PluginResult(PluginResult.Status.NO_RESULT);
+					// TODO Auto-generated method stub
+					try {
+						dbhelper.createdatabase(dbpath);
+						plResult = new PluginResult(PluginResult.Status.OK);
+						callbackContext.sendPluginResult(plResult);
+					}
+					catch (Exception e) {
+						plResult = new PluginResult(PluginResult.Status.ERROR);
+						callbackContext.sendPluginResult(plResult);
+					}
+				}
+			});
+		}     
+    }            
+
+	private void isDatabaseCreated(String dbName, final CallbackContext callbackContext) {
+		final File dbpath;
+		dbname = dbName;
+		final DatabaseHelper dbhelper = new DatabaseHelper(this.cordova.getActivity().getApplicationContext());
+		dbpath = this.cordova.getActivity().getDatabasePath(dbname);
+
+		PluginResult plResult = new PluginResult(PluginResult.Status.NO_RESULT);
+ 		if (dbpath.exists()) {
+			plResult = new PluginResult(PluginResult.Status.OK);
+		}
+		else {
+			plResult = new PluginResult(PluginResult.Status.ERROR);
+		}
+		callbackContext.sendPluginResult(plResult);
     }
 }
